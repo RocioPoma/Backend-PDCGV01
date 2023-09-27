d1adf4072ce2d225274b1f15ea0ebe1893b93090
RocioPomaSilvestre <51862915+RocioPoma@users.noreply.github.com>
2023-09-28 00:15:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const http = require('http');
+/* const http = require('http'); */
 const app = require('./index');
 const server =http.createServer(app);
-server.listen(process.env.PORT);
+server.listen(80);

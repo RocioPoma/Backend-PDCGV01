2038525c7c7b26fbbd1c2b0a5810ec160490ea70
RocioPomaSilvestre <51862915+RocioPoma@users.noreply.github.com>
2023-09-28 03:48:24 +0200

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 var connection = mysql.createConnection({
-    port: 18005,
+/*     port: 18005, */
     host: 'cln27ipjx00p2pmcgsh59jsof',
     user: 'cln27ipjv0c0jcgpmdch2a2m3',
     password: 'W3oeWdjYgm80ncdCpyUrk1vH',

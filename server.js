75a5fd427c1c6bef3b09cf26fad1c4482386de3f
RocioPomaSilvestre <51862915+RocioPoma@users.noreply.github.com>
2023-09-28 03:58:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-/* const http = require('http'); */
-const express = require("express");
-const server = express();
+const http = require('http');
+/* const express = require("express");
+const server = express(); */
 const app = require('./index');
-/* const server =http.createServer(app); */
+const server =http.createServer(app);
 server.listen(80, () => {
   console.log("FUNCIONA")
 });

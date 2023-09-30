c578041dacced0d993fa88bd7a450a9799feb8e5
RocioPomaSilvestre <pomarocio05@gmail.com>
2023-09-30 10:50:18 -0400

    30/09/2023

--- connection.js
+++ connection.js
@@ -2,10 +2,12 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 var connection = mysql.createConnection({
-/*     port: 18005, */
-    host: 'cln27ipjx00p2pmcgsh59jsof',
-    user: 'cln27ipjv0c0jcgpmdch2a2m3',
-    password: 'W3oeWdjYgm80ncdCpyUrk1vH',
+    // port: 18005,
+    // host: '181.188.156.195',
+
+    host: 'cln5c4yri00q6pmcg18ba0o41',
+    user: 'cln5c4yrh0cjzcgpm4icih9ri',
+    password: 'c3oLwWprrgZlX1U7nkdv6ZIC',
     database: 'bdd_proyectos_v03',
 
     // port: 3306,
@@ -15,11 +17,11 @@ var connection = mysql.createConnection({
     // database: 'bdd_proyectos_v03'
 });
 
-connection.connect((err)=>{
-    if(!err){
+connection.connect((err) => {
+    if (!err) {
         console.log("CONNECTED");
     }
-    else{
+    else {
         console.log('ERROR: ');
         console.log(err);
     }

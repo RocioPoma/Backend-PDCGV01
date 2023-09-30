365ac89ff8ffecfa15a4a2a1c5d3b51c7d84b7d4
RocioPomaSilvestre <pomarocio05@gmail.com>
2023-09-30 16:56:23 -0400

    30/09/2023
    Ajustes Accion_Estrategica, Y Documentos

--- routes/acciones_estrategicas.js
+++ routes/acciones_estrategicas.js
@@ -5,7 +5,7 @@ var auth = require('../services/authentication');
 
 //list 
 router.get('/get', (req, res) => {
-  connection.query('SELECT * FROM acciones_estrategicas', (err, results) => {
+  connection.query('SELECT * FROM accion_estrategica', (err, results) => {
     if (err) {
       console.error(err); 
       res.status(500).json({ message: 'Hubo un error al obtener las LINEAS_ESTRATEGICAS' });
@@ -18,7 +18,7 @@ router.get('/get', (req, res) => {
 //obtener acciones estratégicas por id de LineaDeAccion
 router.get('/getByIdLineaDeAccion/:id_linea_accion', (req, res) => {
   const id_linea_accion = req.params.id_linea_accion;
-  var query = 'SELECT ae.* FROM acciones_estrategicas ae WHERE id_linea_accion=?';
+  var query = 'SELECT ae.* FROM accion_estrategica ae WHERE id_linea_accion=?';
   connection.query(query, [id_linea_accion], (err, results) => {
     if (err) {
       console.error(err);
@@ -33,7 +33,7 @@ router.get('/getByIdLineaDeAccion/:id_linea_accion', (req, res) => {
 router.put('/update/:id', (req, res) => {
   const { id } = req.params;
   const { descripcion } = req.body;
-  connection.query('UPDATE acciones_estrategicas SET descripcion = ? WHERE id_acciones_estrategicas = ?', [descripcion, id], (err) => {
+  connection.query('UPDATE accion_estrategica SET descripcion = ? WHERE id_accion_estrategica = ?', [descripcion, id], (err) => {
     if (err) {
       console.error(err);
       res.status(500).json({ message: 'Hubo un error al editar la acción estratégica' });
@@ -48,7 +48,7 @@ router.put('/update/:id', (req, res) => {
 // Habilitar una acción estratégica por ID
 router.put('/activa/:id', (req, res) => {
   const { id } = req.params;
-  connection.query('UPDATE acciones_estrategicas SET estado = 1 WHERE id_acciones_estrategicas = ?', [id], (err) => {
+  connection.query('UPDATE accion_estrategica SET estado = 1 WHERE id_accion_estrategica = ?', [id], (err) => {
     if (err) {
       console.error(err);
       res.status(500).json({ message: 'Hubo un error al habilitar la acción estratégica' });
@@ -61,7 +61,7 @@ router.put('/activa/:id', (req, res) => {
 // Deshabilitar una acción estratégica por ID
 router.put('/desactiva/:id', (req, res) => {
   const { id } = req.params;
-  connection.query('UPDATE acciones_estrategicas SET estado = 0 WHERE id_acciones_estrategicas = ?', [id], (err) => {
+  connection.query('UPDATE accion_estrategica SET estado = 0 WHERE id_accion_estrategica = ?', [id], (err) => {
     if (err) {
       console.error(err);
       res.status(500).json({ message: 'Hubo un error al deshabilitar la acción estratégica' });

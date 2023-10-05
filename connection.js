57ca1cdde5d0c0ec7e4f24f308146202850dff9f
RocioPomaSilvestre <pomarocio05@gmail.com>
2023-10-05 15:39:40 -0400

    05-10-2023

--- connection.js
+++ connection.js
@@ -5,10 +5,10 @@ var connection = mysql.createConnection({
     // port: 18005,
     // host: '181.188.156.195',
 
-     port: 18006,
-     host: '181.188.156.195',
+    //  port: 18006,
+    //  host: '181.188.156.195',
 
-    //host: 'cln5c4yri00q6pmcg18ba0o41',
+    host: 'cln5c4yri00q6pmcg18ba0o41',
     user: 'cln5c4yrh0cjzcgpm4icih9ri',
     password: 'c3oLwWprrgZlX1U7nkdv6ZIC',
     database: 'bdd_proyectos_v03',
